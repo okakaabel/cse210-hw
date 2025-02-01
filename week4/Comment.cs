14c920841967c176543cd307915878ad1575bbda
= <=>
2025-02-01 17:32:07 +0530

    we

---
+++ week4/Comment.cs
@@ -0,0 +1,11 @@
+public class Comment
+{
+    public string CommenterName { get; private set; }
+    public string Text { get; private set; }
+
+    public Comment(string commenterName, string text)
+    {
+        CommenterName = commenterName;
+        Text = text;
+    }
+}
